9e75e822f80428b2c03f443476798e8c623eeb1e
manale el gaamouss <manale.el-gaamouss@ens.uvsq.fr>
2020-04-22 14:11:08 +0200

    insert num

--- JDBC/src/main/java/pglp_5/JDBC/App.java
+++ JDBC/src/main/java/pglp_5/JDBC/App.java
@@ -5,10 +5,14 @@ import java.sql.SQLException;
 
 public class App {
 
-	  public static void main(String[] args) throws IOException, SQLException  {
+	  public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException  {
 		   DAO<Numero_telephone> numjdbc;
-		   Numero_telephone portable =new Numero_telephone(1, "0687447778", "portable");
+		   Numero_telephone portable =new Numero_telephone(1,"062345667", "portable");
+		   Numero_telephone portable1 =new Numero_telephone(2,"056778889", "fix");
 		   numjdbc = DAOFactory.getNumero_telephoneDAO();
-		   numjdbc.create(portable);
+		  // numjdbc.create(portable);
+		  // numjdbc.create(portable1);
+		   numjdbc.find(1);
+		  // numjdbc.delete(portable);
 	  }
 }
--- JDBC/src/main/java/pglp_5/JDBC/Numero_telephoneDAO.java
+++ JDBC/src/main/java/pglp_5/JDBC/Numero_telephoneDAO.java
@@ -8,6 +8,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,19 +23,45 @@ public class Numero_telephoneDAO extends DAO<Numero_telephone>{
 
 	@Override
 	public Numero_telephone create(Numero_telephone obj) throws IOException, SQLException {
-        this.getConnect();
-        String telephoneTABLE = "CREATE TABLE Telephones("
-                + "num_id int,"
-                + "type varchar(30),"
-                + "numero varchar(30),"
-                + "PRIMARY KEY (num_id)"
-                + ")";
-        Statement stmt = getConnect().createStatement();
-        stmt.execute(telephoneTABLE);
-        if(con != null) {
-            
-                con.close();}
-        return obj;
+		  DatabaseMetaData dbmd = getConnect().getMetaData();
+	        ResultSet rs = dbmd.getTables(null, null,"Telephones".toUpperCase(), null);
+	        String createtel="CREATE TABLE Telephones("
+                    + "id_num int,"
+                    + "numero varchar(30),"
+                    + "type varchar(30),"
+                    + "PRIMARY KEY (id_num)"
+                    + ")";
+	         Statement stmt = getConnect().createStatement();
+	            if (!rs.next()) {
+	            	stmt.execute(createtel);
+	            }
+	            
+	                String insertnum= ("insert into Telephones"
+	                        + " values (?, ?, ?)");
+	                PreparedStatement prpstmt = getConnect().prepareStatement(insertnum);
+
+	                prpstmt.setInt(1, obj.getId());
+	                prpstmt.setString(2, obj.getType());
+	                prpstmt.setString(3, obj.getNumero());
+
+	                prpstmt.executeUpdate();
+	                prpstmt.close();
+
+	                rs = stmt.executeQuery("SELECT * FROM Telephones");
+
+	                System.out.println("Table Telephones: \n");
+	                System.out.println("id_num\t type\t numero");
+	                while (rs.next()) {
+	                    System.out.printf("%d\t%s\t%s%n", rs.getInt("id_num"),
+	                            rs.getString("type"), rs.getString("numero"));
+	                }
+	                
+	                System.out.println("\n Table enregistée ");
+	                rs.close();
+	               stmt.close();
+	            
+	            return obj;
+	        
 	}
 
 	@Override
@@ -43,13 +71,16 @@ public class Numero_telephoneDAO extends DAO<Numero_telephone>{
 		   
 		      ResultSet result = this.con.createStatement(
 		        ResultSet.TYPE_SCROLL_INSENSITIVE,
-		        ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Telephones WHERE num_id = " + id);
+		        ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Telephones WHERE id_num = " + id);
+		      System.out.println("Table Telephones: \n");
+              System.out.println("id_num\t type\t numero");
 		      if(result.first())
 		        num = new Numero_telephone(
 		          id,
 		          result.getString("type"),
 		          result.getString("numero"
-		        ));         
+		        )); 
+		      
 		      if(con != null) {
 		    	  con.close();
 	                } 
@@ -81,7 +112,7 @@ public class Numero_telephoneDAO extends DAO<Numero_telephone>{
          stmt.execute("DROP TABLE Telephones");
          if(con != null) {
              
-                 System.out.println("close");
+                 System.out.println("Table supprimée");
                  con.close();}
 	}
 
